f98aa6aa0b5d82850356a463755c2d3b92fd97c7
ChienLin <shzerokimo@hotmail.com>
2022-11-05 14:51:28 +0800

    145165

--- GitTest/src/TEST/Test.java
+++ GitTest/src/TEST/Test.java
@@ -5,7 +5,14 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// test
-
+		Math math =new Math();
+		int sum=math.add(5, 6);
+		System.out.println(sum);
 	}
 
+}
+class Math{
+	int add(int x, int y) {
+		return x+y;
+	}
 }
